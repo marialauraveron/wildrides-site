a3c673ce6fec6c3347a20fb03b99de4fac072a3d
marialauraveron <laurawatfestudio@gmail.com>
2026-02-17 11:27:32 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,9 +5,10 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://dbbmsfrf26.execute-api.us-east-1.amazonaws.com/dev // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://dbbmsfrf26.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 

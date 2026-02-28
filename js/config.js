45d4d10825110f4d1ce934b841b47b3a047f00bd
marialauraveron <laurawatfestudio@gmail.com>
2026-02-28 18:41:32 +0100

    Update AWS Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_zSow78pQS', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6rufqu1l4fe255c2p3859vq08p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_eb8nGD0ao', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3oud2ne938vp60k1lh3mj4rdmf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://dbbmsfrf26.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
